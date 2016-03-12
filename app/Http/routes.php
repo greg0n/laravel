59e7f45feb2da1f8f432cacf5ed3443aa0dda95a
greg0n <grexon5@wp.pl>
2016-03-12 12:51:00 +0100

    I just watched the second movie 'Your First View and Route'

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('about', function () {
+		return view('pages.about'); // resoruces/views/pages/about.blade.php
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
